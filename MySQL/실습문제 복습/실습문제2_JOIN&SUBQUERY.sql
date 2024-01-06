4b685de87ddd796f59d21d5e68f64df73dbb66c8
gwon428 <152463087+gwon428@users.noreply.github.com>
2024-01-06 23:15:43 +0900

    240106

--- MySQL/실습문제 복습/실습문제2_JOIN&SUBQUERY.sql
+++ MySQL/실습문제 복습/실습문제2_JOIN&SUBQUERY.sql
@@ -16,11 +16,52 @@ SELECT * FROM country; -- country_id;
 
 -- 1. first_name이 TRACY인 사람들 조회 
 
+SELECT country, city, address, district, first_name, last_name
+FROM customer
+	JOIN address USING(address_id)
+    JOIN city USING(city_id)
+    JOIN country USING(country_id)
+WHERE first_name = 'TRACY';
+    
+    
 -- 2. 배우 JULIA MCQUEEN이 찍은 영화 제목 조회 (title 기준 정렬 10개까지)
+
+SELECT first_name, last_name, title
+FROM film_actor
+	JOIN film USING (film_id)
+    JOIN actor USING (actor_id)
+WHERE first_name LIKE 'JULIA'
+	AND last_name LIKE 'MCQUEEN'
+ORDER BY 3
+LIMIT 10;
 
 -- 3. 영화 NOON PAPI에 나오는 배우들의 이름 조회
 
+SELECT first_name, last_name, title
+FROM film_actor
+	JOIN film USING (film_id)
+    JOIN actor USING (actor_id)
+WHERE title = 'NOON PAPI';
+
 -- 4. 각 카테고리별 이메일이 JOYCE.EDWARDS@sakilacustomer.org인 고객이 빌린 DVD 대여 수 조회 
 
+SELECT name category, count(*) count
+FROM rental
+	JOIN inventory USING (inventory_id)
+    JOIN customer USING (customer_id)
+    JOIN film_category USING (film_id)
+    JOIN category USING (category_id)
+WHERE email = 'JOYCE.EDWARDS@sakilacustomer.org'
+GROUP BY name;
+
 -- 5. 이메일이 JOYCE.EDWARDS@sakilacustomer.org인 고객이 가장 최근에 빌린 영화 제목과 영화 내용을 조회 
+
+SELECT title, description
+FROM rental
+	JOIN customer USING (customer_id)
+    JOIN inventory USING (inventory_id)
+    JOIN film USING (film_id)
+WHERE email = 'JOYCE.EDWARDS@sakilacustomer.org'
+ORDER BY rental_date DESC
+LIMIT 1;
 

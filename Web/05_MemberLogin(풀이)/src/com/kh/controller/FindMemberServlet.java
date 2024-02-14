a0ee60c259e1416ed1066e926171a8712017faee
gwon428 <152463087+gwon428@users.noreply.github.com>
2024-02-14 23:24:17 +0900

    Revert "240214"
    This reverts commit 4168dabcf41ce54191d658b182e0fd447a436732.

--- Web/05_MemberLogin(풀이)/src/com/kh/controller/FindMemberServlet.java
+++ Web/05_MemberLogin(풀이)/src/com/kh/controller/FindMemberServlet.java
@@ -1,5 +1,4 @@
 package com.kh.controller;
-
 import java.io.IOException;
 import java.sql.SQLException;
 
@@ -10,37 +9,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.kh.model.dao.MemberDAO;
-import com.kh.model.vo.Member;
 
-@WebServlet("/FindMemberServlet")
+@WebServlet("/search")
 public class FindMemberServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void service(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		
-		// 1. 폼값 받아오기
-		String id = request.getParameter("id");
-
-		// 2. DAO 
-		MemberDAO dao = new MemberDAO();
-		try {
-			Member member = dao.findMember(id);
-			
-			if (member != null) {
-				System.out.println("ㅇㄴㄹㄴ");
-				// 3. 바인딩
-				request.setAttribute("member", member);
-				// 4. 네비게이션
-				request.getRequestDispatcher("/views/find_ok.jsp").forward(request, response);
-			} else {
-				// 4. 네비게이션
-				response.sendRedirect("/views/find_fail.jsp");
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	String id = request.getParameter("id");
+	
+	MemberDAO dao = new MemberDAO();
+	try {
+		dao.findMember(id);
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
 	}
 
 }
--- Web/05_MemberLogin(풀이)/src/com/kh/controller/LoginServlet.java
+++ Web/05_MemberLogin(풀이)/src/com/kh/controller/LoginServlet.java
@@ -8,37 +8,23 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import com.kh.model.dao.MemberDAO;
-import com.kh.model.vo.Member;
 
-@WebServlet("/LoginServlet")
+@WebServlet("/Login")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// 1. 폼 값 받아오기
 		String id = request.getParameter("id");
 		String password = request.getParameter("password");
 		
-		// 2. DAO
 		MemberDAO dao = new MemberDAO();
-		
 		try {
-			Member member = dao.LogIn(id, password);
-			
-			// 3. 바인딩 - session
-			HttpSession session = request.getSession();
-			session.setAttribute("member", member);
-			// 4. 네비게이션
-			response.sendRedirect("views/login_result.jsp");
-			
+			dao.LogIn(id, password);
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
-		
 		
 	}
 
